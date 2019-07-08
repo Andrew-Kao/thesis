553128ea2afccef855366b048ed95f038c8c3aae
Andrew Kao <andrewjkao@gmail.com>
2019-07-08 17:17:46 -0500

    figuring out mapping applications to stations

--- explore/Code/instrument/Read_CDBS.R
+++ explore/Code/instrument/Read_CDBS.R
@@ -11,5 +11,20 @@ if (Sys.info()["user"] == "AndrewKao") {
 }
 
 # Structure is cleaned with Haskell to drop extra fields
-ownership_structure <- fread('ownership_structure_clean.dat',header = FALSE, sep = '|', fill=TRUE)
-  
+ownership_structure <- fread('ownership_structure_clean.dat',header = FALSE, sep = '|', fill=TRUE) %>%
+  filter(V5 == "H" | V5 == "N") %>%
+  select(V2, V5) %>%
+  mutate(hispanic = ifelse(V5 == "H", 1, 0)) %>%
+  rename(application_id = V2) %>%
+  group_by(application_id) %>%
+  summarise(count = n(), hisp_sum = sum(hispanic)) %>%
+  mutate(hisp_share = hisp_sum/count)
+
+## join with altc group to get the facility id
+# either V3 or V5 is the key, the other is the facility id
+altc_group <- fread('altc_group.dat', header = FALSE, sep = '|') %>%
+  rename(application_id = V3) %>%
+  right_join(ownership_structure, by = 'application_id')
+
+# checking with this for both V3/V5, we get nothing:
+# sum(!is.na(altc_group$V1)) 
