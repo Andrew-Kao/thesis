a8bb69ace7c305f50279255fed5cb5705a612b6a
Andrew Kao <andrewjkao@gmail.com>
2019-09-02 17:09:12 -0700

    Summary stats/heterogeeneity by location

--- explore/Code/donations/Donor_Merge.R
+++ explore/Code/donations/Donor_Merge.R
@@ -20,8 +20,6 @@ options(stringsAsFactors = FALSE)
 donations <- read.csv('TrumpDonations.csv')
 
 # 2. Name classification data
-# I think we want to try just using the base name, but also try keeping the Hispanic names
-
 FL <- read.csv('names_predict_FL.csv') 
 census <- read.csv('names_predict_census.csv')
 wiki <- read.csv('names_predict_wiki.csv')
@@ -31,15 +29,34 @@ trump <- sf::st_read(dsn='a00000009.gdbtable', layer = 'a00000009') %>%
   filter(Status == "M")  ## only keep precise matches
 trump <- as_Spatial(trump)
 
+# Per Location Data
+locationNames <- donations %>%
+  group_by(contributor_street_1,contributor_city,contributor_state, contributor_zip,contributor_first_name, contributor_last_name) %>%
+  summarise(count = n()) %>%
+  left_join(census, by = c("contributor_first_name","contributor_last_name")) %>%
+  mutate(hisp = count*hispanic, non_hisp = count*black + count*white) %>%
+  group_by(contributor_street_1,contributor_city,contributor_state, contributor_zip) %>%
+  summarise(hisp_sum = sum(hisp), non_hisp_sum = sum(non_hisp)) %>%
+  mutate(race = ifelse(hisp_sum >= non_hisp_sum, 1, 0)) ## race is a dummy for Hispanic
+  
+locationCounts <- donations %>%
+  group_by(contributor_street_1,contributor_city,contributor_state, contributor_zip) %>%
+  summarise(donationCount = n())
+
+
 # Merge
 # Names & Donor Data
 trump@data <- trump@data %>%
   left_join(donations, by = c("street" = "contributor_street_1", "city_1" = "contributor_city",
                               "state2" = "contributor_state", "zip" = "contributor_zip")) %>% # 28,868 NAs
-  left_join(census, by = c("contributor_first_name","contributor_last_name"))  ## 47 NAs (not dropped)
+  left_join(locationNames, by = c("street" = "contributor_street_1", "city_1" = "contributor_city",
+                              "state2" = "contributor_state", "zip" = "contributor_zip")) %>%
+  left_join(locationCounts, by = c("street" = "contributor_street_1", "city_1" = "contributor_city",
+                              "state2" = "contributor_state", "zip" = "contributor_zip"))
 
 saveRDS(trump,file='TrumpAll.Rdata')
 
+####################
   # location mismatches: determining why there are mismatches between locations and donations data
   # unclear for zip 10001 (test[3,])
   # 2/3 don't match either for 10002, one of them is a text parsing difference. Probably no good leads.
