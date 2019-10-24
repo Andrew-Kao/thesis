c1a3b1409166a57c5c0c17b474a95c076edcf7bc
Andrew Kao <andrewjkao@gmail.com>
2019-10-24 00:27:08 -0500

    point pattern begins

--- analysis/Code/Donations.R
+++ analysis/Code/Donations.R
@@ -4,6 +4,8 @@ library(raster)
 library(rgdal)
 library(dplyr)
 library(sf)
+library(stargazer)
+library(stringr)
 
 if (Sys.info()["user"] == "AndrewKao") {
   setwd('~/Documents/College/All/thesis/explore/Data/politics/trump_donations') 
@@ -12,37 +14,48 @@ if (Sys.info()["user"] == "AndrewKao") {
 ## go back to the trump data and recheck this works
 trump <- readRDS(file='TrumpAll.Rdata')
 
-## approach 1: county level spatial regression
+## approach 1: point pattern 
 trump <-spTransform(trump, CRS("+proj=longlat +datum=NAD83"))
+
+### some summary stats
+trumpData <- trump@data %>%
+  select(hisp_sum, non_hisp_sum, race, donationCount)
+stargazer(trumpData, out="../../../Output/Summary/TrumpDonations.tex", title="Trump Donors",
+          summary = TRUE, font.size = 'scriptsize')
+
+### keep only in lower 48
+
+strump <- trump[1:1000,]
+
+## need to match up data to counties and merge
 
 instrument <- readRDS("../../instrument/countyInstrumentCovariate.Rdata")
 counties <- rgdal::readOGR("../../instrument/nhgis0002_shapefile_tl2000_us_county_1990/US_county_1990.shp")
 counties<-spTransform(counties, CRS("+proj=longlat +datum=NAD83"))
 
 ### need a spatial level county dataset
-origCounties <- instrument %>%
+instrument <- instrument %>%
   rename_all(~ paste0("orig",.)) %>%
-  rename(origCty = origcounty, origState = origstate) %>%
-  mutate(origState = str_pad(origState,3,side="left","0"), origCty = str_pad(origCty,3,side="left","0"))
+  rename(COUNTY = origcounty, STATE = origstate) %>%
+  mutate(STATE = str_pad(STATE,3,side="right","0"), COUNTY = str_pad(COUNTY,4,side="right","0"))
 
-destCounties <- instrument %>%
-  rename_all(~ paste0("dest",.)) %>%
-  rename(destCty = destcounty, destState = deststate) %>%
-  mutate(destState = str_pad(destState,3,side="left","0"), destCty = str_pad(destCty,3,side="left","0"))
+### point pattern
+DonationArea <- raster::area(counties)
+allArea <- sum(DonationArea)
+r <- raster(counties)
+res(r) <- 1000 ## try different values? 
 
-## distances
-distances <- fread('../counties/distances/sf12010countydistancemiles.csv') %>%
-  mutate(county1 = str_pad(county1, 6, side = "left", "0")) %>%
-  mutate(county2 = str_pad(county2, 6, side = "left", "0")) %>%
-  mutate(destState = str_sub(county2,start=1,end=3), destCty = str_sub(county2,start=4,end=6)) %>%
-  mutate(origState = str_sub(county1,start=1,end=3), origCty = str_sub(county1,start=4,end=6))
-
-
-counties
-
+### make raster
+### use calc to run regressions?
+r <- rasterize(counties, r)
+plot(r)
+# quads <- as(r, 'SpatialPolygons')
+# plot(quads, add=TRUE)
+points(trump, col='red', cex=.5) ## something has gone very wrong
 
 
-## approach 2: point pattern
+## approach 2: county level spatial regression
+## not preferred because too few counties
 
 
 
