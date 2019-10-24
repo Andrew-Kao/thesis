801a366755ebc7443804a8f6d9af7014f72181f1
Andrew Kao <andrewjkao@gmail.com>
2019-10-24 16:02:50 -0500

    Contour donation links

--- analysis/Code/Donations.R
+++ analysis/Code/Donations.R
@@ -2,6 +2,7 @@
 
 library(raster)
 library(rgdal)
+library(rgeos)
 library(dplyr)
 library(sf)
 library(stargazer)
@@ -23,7 +24,7 @@ trumpData <- trump@data %>%
 stargazer(trumpData, out="../../../Output/Summary/TrumpDonations.tex", title="Trump Donors",
           summary = TRUE, font.size = 'scriptsize')
 
-### keep only in lower 48
+### keep only in lower 48?
 
 strump <- trump[1:1000,]
 
@@ -38,6 +39,38 @@ instrument <- instrument %>%
   rename_all(~ paste0("orig",.)) %>%
   rename(COUNTY = origcounty, STATE = origstate) %>%
   mutate(STATE = str_pad(STATE,3,side="right","0"), COUNTY = str_pad(COUNTY,4,side="right","0"))
+
+### oh god distances to contours now
+contours1 <- readRDS('../../instrument/spanishCountourSLDF.Rdata')
+contours <- spTransform(contours1, CRS("+proj=longlat +datum=NAD83"))
+contours_project <- spTransform(contours1, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+trump_project <- spTransform(trump, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+
+contourTrumpDist <- gDistance(contours_project,trump_project, byid = TRUE)
+contourTrumpMinDist <- apply(contourTrumpDist,1,FUN=min)  # 428 counties that intersect!
+saveRDS(contourTrumpMinDist,'contourTrumpMinDist.Rdata')
+stargazer(matrix(contourTrumpMinDist,ncol=1), out="../../../Output/Summary/ContourTrumpMinDist.tex", title="Contour-Donation Minimum Distances", summary = TRUE)
+
+contours_poly <-SpatialPolygons(
+  lapply(1:length(contours_project), 
+         function(i) Polygons(lapply(coordinates(contours_project)[[i]], function(y) Polygon(y)), as.character(i))))
+crs(contours_poly) <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+contourTrumpIntersect <- gIntersects(contours_poly,trump_project, byid = TRUE)
+contourTrumpIntersect[contourTrumpIntersect == "FALSE"] <- 0
+contourTrumpIntersect[contourTrumpIntersect == "TRUE"] <- 1
+contourTrumpInterAll <- apply(contourTrumpIntersect,1,FUN=sum)  # 590 counties inside/intersecting!
+saveRDS(contourTrumpInterAll,'contourTrumpInterAll.Rdata')
+stargazer(matrix(contourTrumpInterAll,ncol=1), out="../../../Output/Summary/ContourTrumpInterAll.tex", title="Donations Within Contours", summary = TRUE)
+
+trump@data <- trump@data %>%
+  mutate(minDist = contourTrumpMinDist, inside = contourTrumpInterAll)
+
+### merge in county level data
+trumpCountyLink <- over(trump,counties,returnList = FALSE)
+
+
+
+### do the reshape
 
 ### point pattern
 DonationArea <- raster::area(counties)
