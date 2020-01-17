612567af2c054db95272d2ade37c7db4a55549ec
Andrew Kao <andrewjkao@gmail.com>
2020-01-17 10:01:00 -0600

    FL business scrape is the final outcome?

---
+++ explore/Code/firms/Scrape_Florida.R
@@ -0,0 +1,9 @@
+
+# cross year, month, day, and then do in order
+
+yearList <- c(2011:2019)
+monthList <- c(1:12)
+dayList <- c(1:31)
+
+thepage <- readLines('ftp://ftp.dos.state.fl.us/public/doc/cor/20110103c.txt')
+# ftp://ftp.dos.state.fl.us/public/doc/cor/20110103c.txt
