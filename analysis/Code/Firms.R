07a212d9c624d2399727eeae1c33a4effe4eb363
Andrew Kao <andrewjkao@gmail.com>
2020-02-08 03:08:25 -0600

    suspension graph

--- analysis/Code/Firms.R
+++ analysis/Code/Firms.R
@@ -164,6 +164,24 @@ m4 <- glm(hispFoodNameD ~ intersects*distance + intersects*dist2 + logPop + pcHi
 stargazer(m1,m2,m3,m4, out = "../../../Output/Regs/firms_rasterfname2_bin.tex", title="Effect of TV on Binomial Hispanic Name Businesses, 100 KM Radius",
           omit.stat = c('f','ser'), column.sep.width = '-5pt')
 
+regF2 <- regDataF %>%
+  mutate(logPop = log(population), # ceiling(dummy)
+         distance = distance/1000, dist2 = distance^2,
+         hispFoodNameD = ifelse(hispFoodName > 0, 1, 0)) %>%
+  filter(distance < 50)
+m1 <- lm(ihs(busnCount) ~ intersects*distance + intersects*dist2  , data=regF2)
+m2 <- lm(ihs(busnCount) ~ intersects*distance + intersects*dist2 + logPop, data=regF2) 
+m3 <- lm(ihs(busnCount) ~ intersects*distance + intersects*dist2 + logPop + pcHispanic, data=regF2)
+m4 <- lm(ihs(busnCount) ~ intersects*distance + intersects*dist2 + logPop + pcHispanic + income, data=regF2)
+stargazer(m1,m2,m3,m4, out = "../../../Output/Regs/firms_rastern2_ihs_50.tex", title="Effect of TV on IHS Hispanic Owned Businesses, 50 KM Radius",
+          omit.stat = c('f','ser'), column.sep.width = '-5pt')
+m1 <- glm(hispFoodNameD ~ intersects*distance + intersects*dist2  , data=regF2, family = binomial)
+m2 <- glm(hispFoodNameD ~ intersects*distance + intersects*dist2 + logPop, data=regF2, family = binomial) 
+m3 <- glm(hispFoodNameD ~ intersects*distance + intersects*dist2 + logPop + pcHispanic, data=regF2, family = binomial)
+m4 <- glm(hispFoodNameD ~ intersects*distance + intersects*dist2 + logPop + pcHispanic + income, data=regF2, family = binomial)
+stargazer(m1,m2,m3,m4, out = "../../../Output/Regs/firms_rasterfname2_bin_50.tex", title="Effect of TV on Binomial Hispanic Name Businesses, 50 KM Radius",
+          omit.stat = c('f','ser'), column.sep.width = '-5pt')
+
 
 regF2 <- busn2@data %>%
   mutate(logPop = log(origpopulation), # ceiling(dummy)
--- analysis/Code/Plots.R
+++ analysis/Code/Plots.R
@@ -1,16 +1,33 @@
 library(ggplot2)
 
 
+if (Sys.info()["user"] == "AndrewKao") {
+  setwd('~/Documents/College/All/thesis/analysis/Output/graphs') 
+}
+
 # Figures
 
 # Firms
 plotdf <- regDataF %>%
   mutate(inout = ifelse(intersects == 1, 1, -1),
-         distance = distance/1000,
-        dist = inout * distance) %>%
+         distance = distance/1000, dist2 = distance^2,
+        dist = inout * distance,
+        logPop = log(population)) %>%
   filter(distance < 100)
 ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(busnCount)))
-
+ggplot(data = plotdf, aes(x=cut(dist,5),ihs(busnCount))) + geom_boxplot()
+ggplot() + geom_smooth(data = plotdf, aes(dist,hispFoodName))
+ggplot(data = plotdf, aes(dist,hispFoodName)) + geom_smooth(method = lm, formula = y ~ splines::bs(x, 10), se = FALSE)
+m1 <- lm(busnCount ~ logPop + pcHispanic + income + dist2 , data=plotdf)
+pred_busnCount <- predict(m1,plotdf)
+m2 <- lm(hispFoodName ~ logPop + pcHispanic + income + dist2, data=plotdf)
+pred_foodName <- predict(m2,plotdf)
+plotdf <- plotdf %>%
+  mutate(pred_busnCount = pred_busnCount,
+         pred_foodName = pred_foodName)
+ggplot() + geom_smooth(data = plotdf, aes(dist,pred_busnCount))
+ggplot() + geom_smooth(data = plotdf, aes(dist,pred_foodName))
+ggplot(data = plotdf, aes(dist,pred_foodName)) + geom_smooth(method = lm, formula = y ~ splines::bs(x, 5), se = FALSE)
 
 plotdf <- busn2@data %>%
   mutate(inout = ifelse(inside == 1, 1, -1),
@@ -25,9 +42,16 @@ plotdf <- cleanSchoolAll %>%
   mutate(inout = ifelse(inside == 1, 1, -1),
          distance = minDist/1000,
          dist = inout * distance)  %>%
-  filter(distance < 100)
-ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(sch_lepenr_hi)))
+  filter(distance < 50)
+ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(sch_lepenr_hi)), n = 9)
 ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(sch_apenr_hi)))
-ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(sch_discwodis_singoos_hi)))
+ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(sch_discwodis_singoos_hi)), n = 8) +
+  labs(x = "Distance to Contour Boundary (KM)", y = "IHS(Hispanic Students Suspended)") # do w/ 50
+ggsave("hispanicsuspensions.pdf")
+ggplot() + geom_boxplot(data = plotdf, aes(cut(dist,5),ihs(sch_discwodis_singoos_hi)))
+ggplot() + geom_smooth(data = plotdf, aes(dist,ihs(sch_hbreported_rac_hi)), n = 9)
 
+binomial_smooth <- function(...) {
+  geom_smooth(method = "glm", method.args = list(family = "binomial"), ...)
+}
 
