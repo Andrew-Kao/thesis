fba63c58f82baf29bf36f2c8a1aa1dbef97b5c36
Andrew Kao <andrewjkao@gmail.com>
2020-02-07 23:09:45 -0600

    ready to stack

--- analysis/Code/Firms.R
+++ analysis/Code/Firms.R
@@ -53,17 +53,20 @@ crs(contours_poly) <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 
 
 
 #### raster
-r <- raster( xmn =-124.784, xmx=-66.951,ymn=24.743,ymx=49.346,crs= "+proj=longlat +datum=NAD83",
+r <- raster( xmn =-88.47, xmx=-24.2,ymn=-79.4,ymx=31.0,crs= "+proj=longlat +datum=NAD83",
              nrow = 100, ncol = 200)
-rDonCount <- rasterize(trump2, r, field=trump2$donationCount,fun=sum)
-values(rDonCount) <- ifelse(is.na(values(rDonCount)),0,values(rDonCount))
-rHispSum <- rasterize(trump2, r, field=trump2$hisp_sum,fun=mean)
-values(rHispSum) <- ifelse(is.na(values(rHispSum)),0,values(rHispSum))
-rRace <- rasterize(trump2, r, field=trump2$race,fun=mean)
-values(rRace) <- ifelse(is.na(values(rRace)),0,values(rRace))
+rBusnCount <- rasterize(busn, r, field=busn$busnCount,fun=sum)
+values(rBusnCount) <- ifelse(is.na(values(rBusnCount)),0,values(rBusnCount))
+rHispName <- rasterize(busn, r, field=busn$hispName,fun=sum)
+values(rHispName) <- ifelse(is.na(values(rHispName)),0,values(rHispName))
+rHispFoodName <- rasterize(busn, r, field=busn$hispFoodName,fun=sum)
+values(rHispFoodName) <- ifelse(is.na(values(rHispFoodName)),0,values(rHispFoodName))
+rHispNameD <- rasterize(busn, r, field=busn$hispNameD,fun=sum)
+values(rHispNameD) <- ifelse(is.na(values(rHispNameD)),0,values(rHispNameD))
+
 
 #### COUNTIES
-rgdf <- as(rPop,'SpatialGridDataFrame')
+rgdf <- as(rBusnCount,'SpatialGridDataFrame')
 testLink <- over(rgdf,counties,returnList=FALSE)
 rgdf@data <- rgdf@data %>%
   mutate(stateCounty = testLink$stateCounty)
@@ -74,7 +77,7 @@ rPop <- raster(rgdf2,layer=15)
 rIncome <- raster(rgdf2,layer=17)
 
 #### INSTRUMENT
-rspdf <- as(rDonCount,'SpatialPointsDataFrame')
+rspdf <- as(rBusnCount,'SpatialPointsDataFrame')
 rspdf <- spTransform(rspdf, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
 contourDist <- gDistance(contours_project,rspdf, byid = TRUE)
 contourMinDist <- apply(contourDist,1,FUN=min)
