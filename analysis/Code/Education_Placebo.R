3d9676627523ab146b4b2d5b06d332a4d92a4d0c
Andrew Kao <andrewjkao@gmail.com>
2022-02-27 19:20:56 -0500

    placebo contours init

--- analysis/Code/Education_Placebo.R
+++ analysis/Code/Education_Placebo.R
@@ -17,6 +17,12 @@ library(spatialreg)
 library(missForest)
 library(fixest)
 library(conflicted)
+library(sampSurf)
+library(sp)
+library(raster)
+library(rgeos)
+library(rgdal)     
+library(maptools)
 conflict_prefer("select", "dplyr")
 conflict_prefer("filter", "dplyr")
 conflict_prefer("rename", "dplyr")
@@ -31,6 +37,71 @@ if (Sys.info()["user"] == "AndrewKao") {
 
 harass <- read.csv(file = "dda.csv")
 
+# US bounding box
+# (-124.848974, 24.396308) - (-66.885444, 49.384358)
+xmin <- -124.848974
+xmax <- -66.885444
+ymin <- 24.396308
+ymax <- 49.384358
+
+xy <- harass %>%
+  select(Y,X)
+harass_spdf <- SpatialPointsDataFrame(coords = xy, data = harass,
+                                      proj4string = CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+
+set.seed(24601)
+
+placebo_1 <- tibble(coeff = numeric(), se = numeric())
+placebo_2 <- tibble(coeff = numeric(), se = numeric())
+placebo_3 <- tibble(coeff = numeric(), se = numeric())
+
+for (i in 1:1000) {
+  print(i)
+  # generate random TV contours
+  # construct 100 circles
+  for (j in 1:100) {
+    if (j == 1) {
+      new_circle <- spCircle(radius = 2, spUnits = CRS("+proj=longlat +datum=NAD83"),
+                             centerPoint = c(y=runif(1,xmin, xmax),x=runif(1,ymin,ymax)))$spCircle
+    }
+    new_circle <- union(new_circle,
+                        spCircle(radius = 2, spUnits = CRS("+proj=longlat +datum=NAD83"),
+                                 centerPoint = c(y=runif(1,xmin, xmax),x=runif(1,ymin,ymax)))$spCircle)
+  }
+  crs(new_circle) <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+  contourLEAIntersect <- gIntersects(new_circle,harass_spdf, byid = TRUE)
+  contourLEAIntersect[contourLEAIntersect == "FALSE"] <- 0
+  contourLEAIntersect[contourLEAIntersect == "TRUE"] <- 1
+  contourLEAInterAll <- apply(contourLEAIntersect,1,FUN=sum)  
+  placebo_tv <- harass %>%
+    mutate(TV = contourLEAInterAll)
+  summary(placebo_tv$TV)
+  
+  # run regressions and store results
+  om1 <- feols(ihs(sch_satact) ~ TV*eth +
+                 origpcHisp + origLogInc + origLogPop + SCH_TEACHERS_CURR_TOT +  hisp_students + asian_students  + 
+                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID"), data=placebo_tv,
+               subset = harass$origdist < 100)
+  placebo_1 <- placebo_1 %>% 
+    add_row(coeff = om1$coefficients['TV:eth'], se = om1$se['TV:eth'])
+  
+  om2 <- feols(ihs(sch_mathenr_calc) ~ TV*eth +
+                 origpcHisp + origLogInc + origLogPop + SCH_TEACHERS_CURR_TOT +  hisp_students + asian_students  + 
+                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID"), data=placebo_tv,
+               subset = harass$origdist < 100)
+  placebo_2 <- placebo_2 %>% 
+    add_row(coeff = om2$coefficients['TV:eth'], se = om2$se['TV:eth'])
+  
+  om3 <- feols(ihs(sch_appass_oneormore) ~ TV*eth +
+                 origpcHisp + origLogInc + origLogPop + SCH_TEACHERS_CURR_TOT +  hisp_students + asian_students  + 
+                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID"), data=placebo_tv,
+               subset = harass$origdist < 100)
+  placebo_3 <- placebo_3 %>% 
+    add_row(coeff = om3$coefficients['TV:eth'], se = om3$se['TV:eth'])
+}
+
+
+
 # plan:
 # 1. draw fake contour
 # https://search.r-project.org/CRAN/refmans/sampSurf/html/spCircle.html 
@@ -39,6 +110,9 @@ harass <- read.csv(file = "dda.csv")
 # 4. repeat 1-3 1000x
 # 5. plot result
 
+
+# radius 250km 
+# https://www.fcc.gov/media/radio/fm-and-tv-propagation-curves
 
 
 ###### alternate placebos:
