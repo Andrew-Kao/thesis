6dfa7ca6bd1c4a9b425a2431b1140861a7587691
Andrew Kao <andrewjkao@gmail.com>
2022-03-11 16:19:34 -0500

    aefp prep, plus ANES irb init

--- analysis/Code/Education_Placebo.R
+++ analysis/Code/Education_Placebo.R
@@ -35,6 +35,7 @@ if (Sys.info()["user"] == "AndrewKao") {
 }
 
 
+## TODO: dda that's not just the sample
 harass <- read.csv(file = "dda.csv")
 
 
@@ -83,38 +84,118 @@ for (i in 1:1000) {
                                  centerPoint = c(y=runif(1,xmin, xmax),x=runif(1,ymin,ymax)))$spCircle)
   }
   crs(new_circle) <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+  
+  contourLEADist <- gDistance(new_circle,harass_spdf, byid = TRUE)
+  contourLEAMinDist <- apply(contourLEADist,1,FUN=min)  # 428 counties that intersect!
+  
   contourLEAIntersect <- gIntersects(new_circle,harass_spdf, byid = TRUE)
   contourLEAIntersect[contourLEAIntersect == "FALSE"] <- 0
   contourLEAIntersect[contourLEAIntersect == "TRUE"] <- 1
   contourLEAInterAll <- apply(contourLEAIntersect,1,FUN=sum)  
   placebo_tv <- harass %>%
-    mutate(TV = contourLEAInterAll)
+    mutate(TV = contourLEAInterAll, minDist = contourLEAMinDist) %>%
+    filter(minDist < 1)
   summary(placebo_tv$TV)
   
   # run regressions and store results
   om1 <- feols(ihs(sch_satact) ~ TV*eth +
                  origpcHisp + origLogInc + origLogPop + SCH_TEACHERS_CURR_TOT +  hisp_students + asian_students  + 
-                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID"), data=placebo_tv,
+                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID","eth"), data=placebo_tv,
                subset = harass$origdist < 100)
   placebo_1 <- placebo_1 %>% 
     add_row(coeff = om1$coefficients['TV:eth'], se = om1$se['TV:eth'])
   
   om2 <- feols(ihs(sch_mathenr_calc) ~ TV*eth +
                  origpcHisp + origLogInc + origLogPop + SCH_TEACHERS_CURR_TOT +  hisp_students + asian_students  + 
-                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID"), data=placebo_tv,
+                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID","eth"), data=placebo_tv,
                subset = harass$origdist < 100)
   placebo_2 <- placebo_2 %>% 
     add_row(coeff = om2$coefficients['TV:eth'], se = om2$se['TV:eth'])
   
   om3 <- feols(ihs(sch_appass_oneormore) ~ TV*eth +
                  origpcHisp + origLogInc + origLogPop + SCH_TEACHERS_CURR_TOT +  hisp_students + asian_students  + 
-                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID"), data=placebo_tv,
+                 total_students + SCH_GRADE_G01 + SCH_GRADE_G06 + SCH_GRADE_G09 | LEAID, cluster = c("LEAID","eth"), data=placebo_tv,
                subset = harass$origdist < 100)
   placebo_3 <- placebo_3 %>% 
     add_row(coeff = om3$coefficients['TV:eth'], se = om3$se['TV:eth'])
 }
 
+## make plots
 
+placebo_1 <- placebo_1 %>%
+  mutate(top = coeff + 1.96 * se, bottom = coeff - 1.96 * se) %>%
+  arrange(coeff) %>%
+  mutate(x = 1:nrow(placebo_1))
+
+placebo_1_s <- placebo_1 %>%
+  filter((top > 0 & bottom > 0) | (top < 0 & bottom < 0))
+placebo_1_ns <- placebo_1 %>%
+  filter((top > 0 & bottom < 0))
+placebo_1_real <- tibble(coeff = 0.1598, top = 0.1598 + 1.96 * 0.0264, bottom = 0.1598 - 1.96 * 0.0264,
+                         x = 51:52)
+
+ggplot(placebo_1_ns, aes(x = x, y = coeff)) +
+  geom_point() +
+  geom_ribbon(data = placebo_1, aes(ymin = bottom, ymax = top),
+              alpha = 0.1) +
+  geom_point(data = placebo_1_s, aes(x = x, y = coeff, color = "red")) +
+  geom_ribbon(data = placebo_1_real, aes(x=x,ymin = bottom, ymax = top)) +
+  geom_point(data = placebo_1_real, aes(x = x, y = coeff, color = "blue")) +
+  labs(y = "Coefficient", x = "IHS(SAT/ACTs taken)") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  scale_color_manual(labels = c("actual","p < .05"), values = c("blue","red"))
+ggsave("../../../analysis/Output/graphs/placebo_contour_1.pdf")
+
+placebo_2 <- placebo_2 %>%
+  mutate(top = coeff + 1.96 * se, bottom = coeff - 1.96 * se) %>%
+  arrange(coeff) %>%
+  mutate(x = 1:nrow(placebo_2))
+
+placebo_2_s <- placebo_2 %>%
+  filter((top > 0 & bottom > 0) | (top < 0 & bottom < 0))
+placebo_2_ns <- placebo_2 %>%
+  filter((top > 0 & bottom < 0))
+placebo_2_real <- tibble(coeff = 0.2718, top = 0.2718 + 1.96 * 0.0369, bottom = 0.2718 - 1.96 * 0.0369,
+                         x = 1001:1002)
+
+ggplot(placebo_2_ns, aes(x = x, y = coeff)) +
+  geom_point() +
+  geom_ribbon(data = placebo_2, aes(ymin = bottom, ymax = top),
+              alpha = 0.1) +
+  geom_point(data = placebo_2_s, aes(x = x, y = coeff, color = "red")) +
+  geom_ribbon(data = placebo_2_real, aes(x=x,ymin = bottom, ymax = top)) +
+  geom_point(data = placebo_2_real, aes(x = x, y = coeff, color = "blue")) +
+  labs(y = "Coefficient", x = "IHS(calculus taken)") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  scale_color_manual(labels = c("actual","p < .05"), values = c("blue","red"))
+ggsave("../../../analysis/Output/graphs/placebo_contour_2.pdf")
+
+placebo_3 <- placebo_3 %>%
+  mutate(top = coeff + 1.96 * se, bottom = coeff - 1.96 * se) %>%
+  arrange(coeff) %>%
+  mutate(x = 1:nrow(placebo_3))
+
+placebo_3_s <- placebo_3 %>%
+  filter((top > 0 & bottom > 0) | (top < 0 & bottom < 0))
+placebo_3_ns <- placebo_3 %>%
+  filter((top > 0 & bottom < 0))
+placebo_3_real <- tibble(coeff = 0.0964, top = 0.0964 + 1.96 * 0.0346, bottom = 0.0964 - 1.96 * 0.0346,
+                         x = 934:935)
+
+ggplot(placebo_3_ns, aes(x = x, y = coeff)) +
+  geom_point() +
+  geom_ribbon(data = placebo_3, aes(ymin = bottom, ymax = top),
+              alpha = 0.1) +
+  geom_point(data = placebo_3_s, aes(x = x, y = coeff, color = "red")) +
+  geom_ribbon(data = placebo_3_real, aes(x=x,ymin = bottom, ymax = top)) +
+  geom_point(data = placebo_3_real, aes(x = x, y = coeff, color = "blue")) +
+  labs(y = "Coefficient", x = "IHS(APs passed)") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  scale_color_manual(labels = c("actual","p < .05"), values = c("blue","red"))
+ggsave("../../../analysis/Output/graphs/placebo_contour_3.pdf")
 
 ###### alternate placebos:
 # randomly draw schools 
